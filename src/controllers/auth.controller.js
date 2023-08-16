a40bcfebc063d9da158c56030c1e9c3987beb658
santiago <santithevenetvalles@gmail.com>
2023-08-16 09:25:02 +0200

    Verify session

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import { createAccessToken } from "../libs/jwt.js";
 import User from "../models/user.model.js";
+import jwt from 'jsonwebtoken'
+import { TOKEN_SECRET } from "../config.js";
 
 
 export const register = async (req, res) => {
@@ -57,4 +59,24 @@ export const logout = async (req, res) => {
     })
 
     return res.sendStatus(200)
+}
+
+export const verifyToken = async (req, res) => {
+    const {token} = req.cookies
+    
+    if (!token) return res.status(401).json({message: "Unauthorize"})
+
+    jwt.verify(token, TOKEN_SECRET, async (err, user) => {
+        if (err) return res.status(401).json({message: "Unauthorized"}) 
+
+        const userFound = await User.findById(user.id)
+
+        if (!userFound) return res.status(401).json({message: "Unauthorized"})
+
+        return res.json({
+            id: userFound._id,
+            username: userFound.username,
+            email: userFound.email
+        })
+    })
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { login, logout, register } from "../controllers/auth.controller.js";
+import { login, logout, register, verifyToken } from "../controllers/auth.controller.js";
 import { registerSchema, loginSchema } from "../schemas/auth.schema.js";
 import { validateSchema } from "../middlewares/validator.middleware.js";
 
@@ -8,5 +8,6 @@ const router = Router()
 router.post('/register', validateSchema(registerSchema), register)
 router.post('/login', validateSchema(loginSchema), login)
 router.post('/logout', logout)
+router.get('/verify', verifyToken)
 
 export default router
